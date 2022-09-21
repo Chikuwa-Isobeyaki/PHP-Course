403529a701c7bf76f861ed6eea4e0454f613cad2
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-09-21 21:03:40 +0900

    [Finish]オブジェクトで日付を扱う

--- lesson01/sample04.php
+++ lesson01/sample04.php
@@ -3,4 +3,8 @@
 date_default_timezone_set('Asia/Tokyo');
 
 echo date('G時 i分 s秒');
+
+// 文字列と数値は.で連結させることができる
+echo '現在は、' . date('G時 i分 s秒') . 'です。';
+echo '現在は、', date('G時 i分 s秒'), 'です。';
 ?>
--- lesson01/sample05.php
+++ lesson01/sample05.php
@@ -1,8 +1,5 @@
 <?php
-// タイムゾーンを設定
-date_default_timezone_set('Asia/Tokyo');
-
-// 文字列と数値は.で連結させることができる
-echo '現在は、' . date('G時 i分 s秒') . 'です。';
-echo '現在は、', date('G時 i分 s秒'), 'です。';
+$today = new DateTime();
+$today->setTimezone(new DateTimeZone('Asia/Tokyo'));
+echo '現在は' . $today->format('G秒 i分 s秒') . 'です。';
 ?>
