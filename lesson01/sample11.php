b07f99b9ef58f57e06ce5b18a04889f9fed5dc7f
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-09-23 17:11:19 +0900

    [Finish]連想配列

---
+++ lesson01/sample11.php
@@ -0,0 +1,18 @@
+<?php
+//連想配列
+$fruits = [
+  'apple' => 'りんご',
+  'grape' => 'ぶどう',
+  'lemon' => 'レモン',
+  'tomato' => 'トマト',
+  'peach' => 'もも'
+];
+?>
+
+<dl>
+  <?php foreach ($fruits as $english => $japanese): ?>
+    <dt><?php echo $english; ?></dt>
+    <dd><?php  echo $japanese; ?></dd>
+  <?php endforeach; ?>
+</dl>
+
