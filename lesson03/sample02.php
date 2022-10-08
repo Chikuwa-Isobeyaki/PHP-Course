d3958c9a94018f622af7c4032155938aca4798df
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-10-08 22:48:19 +0900

    [Finish]count

--- lesson03/sample02.php
+++ lesson03/sample02.php
@@ -9,11 +9,11 @@
 <body>
   <?php
   $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
-  $records = $db->query('select * from my_items');
+  $records = $db->query('select count(*) as cnt from my_items');
   if ($records) {
     // fetch_assoc()で連想配列として取り出す
     while ($record = $records->fetch_assoc()) {
-      echo $record['item_name'] . '<br>';
+      echo $record['cnt'] . '<br>';
     }
   } else {
     echo $db->error;
